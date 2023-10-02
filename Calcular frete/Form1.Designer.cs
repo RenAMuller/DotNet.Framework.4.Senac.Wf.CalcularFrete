2496550d5103e75d255b9b127f03b9d2a62f181a
Renan Augusto <renan.muller8@gmail.com>
2023-10-02 20:49:41 -0300

    alteração do projeto

--- Calcular frete/Form1.Designer.cs
+++ Calcular frete/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 65);
+            this.button1.Location = new System.Drawing.Point(78, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
